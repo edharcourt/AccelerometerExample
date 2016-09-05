a807f40e50578e982e9454718dc96d1506a63983
Ed Harcourt <ehar@ehar-gpu-project.stlawu.local>
2016-09-05 17:32:59 -0400

    add handler for location

---
+++ app/src/main/java/com/example/ehar/SensorExample/LocationHandler.java
@@ -0,0 +1,96 @@
+package com.example.ehar.SensorExample;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import java.util.List;
+import java.util.Observable;
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by ehar on 9/5/16.
+ */
+public class LocationHandler extends Observable implements LocationListener {
+
+    Activity act;
+
+    LocationManager lm;
+
+    public LocationHandler(Activity act) {
+        this.act = act;
+
+        if (this.act.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this.act,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},
+                    MainActivity2.REQUEST_ASK_FINE_LOCATION
+            );
+        }
+
+    }
+
+    public void initializeLocationManager() {
+        lm = (LocationManager)this.act.getApplicationContext().getSystemService(act.LOCATION_SERVICE);
+
+        if (act.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
+
+            // I don't think you are supposed to manually call onLocationChanged
+            setChanged();
+            notifyObservers(getLastKnownLocation());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setChanged();
+        notifyObservers(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) { }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+    /**
+     * Borrowed from StackOverflow
+     * http://stackoverflow.com/questions/20438627/getlastknownlocation-returns-null
+     * @return
+     */
+    private Location getLastKnownLocation() {
+
+        List<String> providers = lm.getProviders(true);
+        Location bestLocation = null;
+        if (act.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            for (String provider : providers) {
+                Location l = lm.getLastKnownLocation(provider);
+                if (l == null) {
+                    continue;
+                }
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    // Found best last known location: %s", l);
+                    bestLocation = l;
+                }
+            }
+        }
+        return bestLocation;
+    }
+
+
+}
--- app/src/main/java/com/example/ehar/SensorExample/MainActivity2.java
+++ app/src/main/java/com/example/ehar/SensorExample/MainActivity2.java
@@ -1,11 +1,16 @@
 package com.example.ehar.SensorExample;
 
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -20,13 +25,9 @@ public class MainActivity2
     // textviews
     private TextView accel_y_view = null;
     private Observable accel;
+    private LocationHandler location;
 
-    @Override
-    public void update(Observable observable, Object o) {
-        float [] values = (float []) o;
-        accel_y_view.setText(Float.toString(values[1]));
-    }
-
+    final public static int REQUEST_ASK_FINE_LOCATION = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +37,49 @@ public class MainActivity2
         accel_y_view = (TextView) findViewById(R.id.accel_y);
         this.accel = new AccelerometerHandler(500, this);
         this.accel.addObserver(this);
+
+        this.location = new LocationHandler(this);
+        this.location.addObserver(this);
     }
+
+    @Override
+    public void update(Observable observable, Object o) {
+
+        if (observable instanceof AccelerometerHandler) {
+            float[] values = (float[]) o;
+            accel_y_view.setText(Float.toString(values[1]));
+        }
+        else if (observable instanceof LocationHandler) {
+            Location l = (Location) o;
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+
+            Toast.makeText(MainActivity2.this, "Lat: " + lat +
+                    " Lon: " + lon, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_ASK_FINE_LOCATION)
+            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Log.i("INFO: ", "Permission not granted for location.");
+            }
+            else {
+                Log.i("INFO: ", "Permission granted for location.");
+                location.initializeLocationManager();
+            }
+    }
 }
